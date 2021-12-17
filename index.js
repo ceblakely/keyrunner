64238683d101487a10a992ec80ec64d9993bfc8c
ceblakely <cblakelydev@gmail.com>
2021-12-17 00:52:01 -0500

    Fixed backspace function & css

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ let completedWords = []; // array
 let currentWord = "";
 let currentIndex = 0;
 let wordIndex = 0;
-let time = 60;
+let time = 10;
 let totalKeystrokes = 0;
 let wordsField = document.getElementById("wordsField");
 
 let userInfo = {};
 const sentences = [
-  "christinablakely how was dog mail cat ear drink home glass cake water car textbook baseboards lackhack whichever yellow stargaze snow day school park anteater blue television selling astonishing indoor reasonable embryo gasp background systematic functional final case behavior replacement resource needle marketing novel west structure fang redundant snail grouchy tendency badge legs bloody grubby imagine nappy sedate murmur lumpy good sleep fall existence knit connotesting melodic die request push kid sour disease houses drain dinosaurs mislead legrail violent flock minetray forgive drunk",
-  "christinablakely website pen paper agenda toy baby dinner music circle team interview influence ring cleaner fluffy charger planner cup mouse sticker tree nature edge fan mosquito soar prevalence state instinct snow utter spit preoccupation window key height invasion ostracize mean contradiction category hypothesis routine ghostwriter pat stress happen image fierce religion toe thin blush illustrious inhabit apparel amount event ooze recondite escape beneficial insect jewel thumbs way shut board splendid burn burly prove illumine pet womanly degree imply canvass",
-  "christinablakely shoe book ring plug garden draw mailbox mouse charge food assist author became flight editor eating doctor engine empire dollar winner tennis ticket window writer yellow concert color-blind watch sour positive sentence disposition common do overeat hell colon plagiarize knot wire orgy research credibility hunter appearance veteran steep fight mile shake list glance obsolete branch neat string groovy boiling thirsty violates link satiated i gestion amazing reply ring tree enchanted rend restrain chain kind hug attract detailed cease rude worship ant lumber daffy suffer combative numerous deadpan giants snails aunt appear",
-  "christinablakely tree bird movie heel dog truck soccer game polish color share sports shopping travel money weather computer average inject meadow quote ruin dark lesson team problem crude body concert contrast wool leap hypnotize creature survive help horses mouth fancy breathe stop typical wet zoom gabby blowhuge perfect suggestions alty successful help less married consign wary applegarden blow lively chance cheap sour legal taste part suggest allow outrageous compel giant crabby game word respect race stage modify jittery wave few secretary disillusioned perpetual verify press watch join consult bat feeble curly joyous  cry milky lavish colossal apathetic migrate fish",
+  "christina blakely how was dog mail cat ear drink home glass cake water car textbook baseboards lackhack whichever yellow stargaze snow day school park anteater blue television selling astonishing indoor reasonable embryo gasp background systematic functional final case behavior replacement resource needle marketing novel west structure fang redundant snail grouchy tendency badge legs bloody grubby imagine nappy sedate murmur lumpy good sleep fall existence knit connotesting melodic die request push kid sour disease houses drain dinosaurs mislead legrail violent flock minetray forgive drunk",
+  "christina blakely website pen paper agenda toy baby dinner music circle team interview influence ring cleaner fluffy charger planner cup mouse sticker tree nature edge fan mosquito soar prevalence state instinct snow utter spit preoccupation window key height invasion ostracize mean contradiction category hypothesis routine ghostwriter pat stress happen image fierce religion toe thin blush illustrious inhabit apparel amount event ooze recondite escape beneficial insect jewel thumbs way shut board splendid burn burly prove illumine pet womanly degree imply canvass",
+  "christina blakely shoe book ring plug garden draw mailbox mouse charge food assist author became flight editor eating doctor engine empire dollar winner tennis ticket window writer yellow concert color-blind watch sour positive sentence disposition common do overeat hell colon plagiarize knot wire orgy research credibility hunter appearance veteran steep fight mile shake list glance obsolete branch neat string groovy boiling thirsty violates link satiated i gestion amazing reply ring tree enchanted rend restrain chain kind hug attract detailed cease rude worship ant lumber daffy suffer combative numerous deadpan giants snails aunt appear",
+  "christina blakely tree bird movie heel dog truck soccer game polish color share sports shopping travel money weather computer average inject meadow quote ruin dark lesson team problem crude body concert contrast wool leap hypnotize creature survive help horses mouth fancy breathe stop typical wet zoom gabby blowhuge perfect suggestions alty successful help less married consign wary applegarden blow lively chance cheap sour legal taste part suggest allow outrageous compel giant crabby game word respect race stage modify jittery wave few secretary disillusioned perpetual verify press watch join consult bat feeble curly joyous  cry milky lavish colossal apathetic migrate fish",
 ];
 
 function getKey(e) {
@@ -82,15 +82,16 @@ function colorText(key) {
 function backspaceKey() {
   if (
     userInfo.lastKeys.at(-1) ==
-    '<span style="background-color: #1b998b" class="newLetter"> </span>'
+    '<span style="background-color: #008F6B" class="newLetter"> </span>'
   ) {
-    //let lastWord = userInfo.lastWords.pop();
-    let lastWord = userInfo.colorLastWords.pop(-2);
-    currentIndex = currentIndex - 2;
+    userInfo.lastKeys.pop();
+    userInfo.valArr.unshift(" ");
+    userInfo.newStringArr = userInfo.colorLastWords.pop();
+
     document.getElementById("inputField").value = userInfo.lastWords.pop();
     document.getElementById("wordsField").innerHTML =
-      lastWord + " " + userInfo.valArr.join("");
-    userInfo.valArr.unshift(userInfo.gameString[currentIndex]);
+      userInfo.newStringArr + " " + userInfo.valArr.join("");
+    currentIndex = currentIndex - 1;
   } else {
     currentWord = currentWord.slice(0, currentWord.length - 66);
     userInfo.newStringArr = userInfo.newStringArr.slice(
@@ -99,32 +100,10 @@ function backspaceKey() {
     );
     currentIndex = currentIndex - 1;
     userInfo.valArr.unshift(userInfo.gameString.at(currentIndex));
-    //console.log(userInfo.valArr);
     document.getElementById("wordsField").innerHTML =
       userInfo.newStringArr + userInfo.valArr.join("");
     userInfo.lastKeys.pop(-1);
   }
-  //let lastKey = userInfo.lastKeys.pop();
-  //console.log(lastKey);
-  //currentIndex = currentIndex - 1;
-
-  //userInfo.newStringArr = userInfo.colorLastWords.pop();
-
-  //userInfo.newStringArr = userInfo.newStringArr.slice(
-  //-1
-  //userInfo.newStringArr.length - lastLen
-  /*
-  if (lastKey == " ") {
-    let lastWord = userInfo.lastWords.pop();
-    console.log(lastWord);
-    currentIndex = currentIndex - 2;
-    document.getElementById("inputField").value = lastWord;
-  } else {
-    currentIndex -= currentIndex;
-    document.getElementById("wordsField").innerHTML =
-      currentWord + valArr.join("");
-  } */
-  //insertNew(userInfo.newStringArr, userInfo.valArr);
 }
 
 function insertNew(newStringArr, valArr, space = false) {
@@ -141,88 +120,54 @@ function insertNew(newStringArr, valArr, space = false) {
 function isCorrect(key, currentLetter) {
   if (key == currentLetter) {
     return (
-      '<span style="background-color: #1b998b" class="newLetter">' +
+      '<span style="background-color: #008F6B" class="newLetter">' +
       currentLetter +
       "</span>"
     );
   } else {
     userInfo.errors++;
     return (
-      '<span style="background-color: #a31621" class="newLetter">' +
+      '<span style="background-color: #A44A3F" class="newLetter">' +
       currentLetter +
       "</span>"
     );
   }
 }
-/*
-function displayRoundResults() {
-  let scores = getScore();
-  userInfo[currentRound].wpm = scores[0];
-  userInfo[currentRound].accuracy = Math.round(scores[1]);
-
-  document.getElementById("mainP").setAttribute("class", "roundInfoStyle");
-  document.getElementById("mainP").innerHTML =
-    "<h4>Round " +
-    currentRound +
-    " Complete</h4><hr><p>Errors: " +
-    userInfo[currentRound].errors +
-    "<p> WPM: " +
-    userInfo[currentRound].wpm +
-    "</p><p> Accuracy: " +
-    userInfo[currentRound].accuracy +
-    "%</p>";
-}
-/*
-  if (currentRound < 3) {
-    document.getElementById("startBtn").innerText = "Next Round";
+function calcScore() {
+  let wpm = userInfo.lastKeys.length / 5;
+  let totalCorrect = 0;
+  let accuracy;
+  let rank;
+  let saying;
+  userInfo.lastKeys.forEach((element) => {
+    if (element.includes("#008F6B")) {
+      totalCorrect++;
+    }
+  });
+  if (wpm <= 40) {
+    rank = "Below Average";
+    saying = "Keep practicing your skills!";
+  } else if (wpm > 40 && wpm <= 70) {
+    rank = "Above Average!";
+    saying = "Not too shabby!";
   } else {
-    document.getElementById("startBtn").innerText = "See Final Score";
-    document.getElementById("startBtn").addEventListener("click", finalScore);
+    rank = "Excellent!";
+    saying = "You are a typing genius, congratulations!";
   }
-  showElements([document.getElementById("startBtn")]);
+  accuracy = Math.round((totalCorrect / userInfo.lastKeys.length) * 100);
+  document.getElementById("wordsField").innerHTML =
+    "<h2>Rank: " +
+    rank +
+    "</h2><br>" +
+    "</h3><br><p>Words Per Minute: " +
+    wpm +
+    "<br> Accuracy: " +
+    accuracy +
+    "% <br> Total Errors: " +
+    userInfo.errors +
+    "</p>";
 }
 
-function getScore() {
-  let seconds =
-    (userInfo[currentRound].endTime - userInfo[currentRound].startTime) / 1000;
-  let minutes = seconds / 60;
-  console.log(Math.round(seconds) + " seconds");
-  let wpm = Math.floor(userInfo[currentRound].gameString.length / 5 / minutes);
-  let accuracy =
-    ((userInfo[currentRound].gameString.length -
-      userInfo[currentRound].errors) /
-      userInfo[currentRound].gameString.length) *
-    100;
-  console.log(wpm, accuracy);
-
-  return [wpm, accuracy];
-}
-function finalScore() {
-  let totalErrors =
-    userInfo[1].errors + userInfo[2].errors + userInfo[3].errors;
-  let averageWpm = Math.round(
-    (userInfo[1].wpm + userInfo[2].wpm + userInfo[3].wpm) / 3
-  );
-  let averageAcc = Math.round(
-    (userInfo[1].accuracy + userInfo[2].accuracy + userInfo[3].accuracy) / 3
-  );
-
-  document.getElementById("mainP").setAttribute("class", "roundInfoStyle");
-  document.getElementById("mainP").innerHTML =
-    "<h4>Final Score</h4><hr><p>Total Errors: " +
-    totalErrors +
-    "<p> Average WPM: " +
-    averageWpm +
-    "</p><p> Average Accuracy: " +
-    averageAcc +
-    "%</p>";
-}
-
-
-function startNewRound() {
-  document.getElementById("inputField").value = "";
-}
-*/
 /* Below this is good */
 window.onload = function () {
   randomSentence = getSentence(Math.floor(Math.random() * sentences.length));
@@ -232,7 +177,7 @@ window.onload = function () {
   userInfo.eachWordArr = randomSentence.split(" ");
   userInfo.newStringArr = "";
   userInfo.errors = 0;
-  //userInfo.lenArr = [];
+
   userInfo.lengthVal = 66;
   userInfo.lastKeys = [];
   userInfo.lastWords = [];
@@ -266,6 +211,10 @@ function countdown_timer() {
     }
   } else {
     document.getElementById("countdownClock").innerHTML = "0:00";
+    document.getElementById("inputField").setAttribute("class", "hide");
+    document.getElementById("playAgain").removeAttribute("class", "hide");
     clearInterval(countdown_timer);
+    calcScore();
   }
+  document.getElementById("inputField").removeAttribute("placeholder");
 }
